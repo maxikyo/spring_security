bbd4fba62fb28902d6e5475d48aa3cbbe6783e75
maxikyo <holovanovmax@gmail.com>
2023-04-29 01:03:50 +0200

    create till Post, has to be updated

--- src/main/groovy/com/holovanovmax/springboot_security_api/rest_api/controller/Controller.groovy
+++ src/main/groovy/com/holovanovmax/springboot_security_api/rest_api/controller/Controller.groovy
@@ -4,6 +4,9 @@ import com.holovanovmax.springboot_security_api.rest_api.model.User
 import com.holovanovmax.springboot_security_api.rest_api.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,10 +14,21 @@ class Controller {
     @Autowired
     private UserService userService
 
-    @GetMapping("/showusers")
-    List<User> showAllUsers(){
+    @GetMapping("/users")
+    List<User> showAllUsers() {
         List<User> allUsers = userService.getAllUsers()
         return allUsers
+    }
+
+    @GetMapping("/users/{id}")
+    User getUsers(@PathVariable String id) {
+        User user = userService.getUsers(id)
+        return user
+    }
+
+    @PostMapping("/users")
+    void addUsers(@RequestBody List<User> users){
+        userService.saveUsers(users)
     }
 
 }
