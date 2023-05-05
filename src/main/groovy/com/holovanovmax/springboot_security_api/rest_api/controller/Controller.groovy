5855196bd269cf6b323b9ba0a0db903a9833f686
maxikyo <holovanovmax@gmail.com>
2023-05-05 19:43:11 +0200

    create controller for FindByName

--- src/main/groovy/com/holovanovmax/springboot_security_api/rest_api/controller/Controller.groovy
+++ src/main/groovy/com/holovanovmax/springboot_security_api/rest_api/controller/Controller.groovy
@@ -2,13 +2,18 @@ package com.holovanovmax.springboot_security_api.rest_api.controller
 
 import com.holovanovmax.springboot_security_api.rest_api.model.User
 import com.holovanovmax.springboot_security_api.rest_api.service.UserService
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+@Slf4j
 @RestController
 class Controller {
     @Autowired
@@ -31,6 +36,24 @@ class Controller {
         return user
     }
 
+    @DeleteMapping("/users/{id}")
+    ResponseEntity deleteProductById(@PathVariable String id) {
+        Optional<User> product = this.userService.deleteUser(id)
+
+        if (product.isPresent()) {
+            this.userService.deleteUser(id)
+            return ResponseEntity.ok("Done")
+        } else {
+            log.warn('user with id ' + id + ' not found! And did not deleted')
+            return new ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @GetMapping("/users")
+    List<User> findAllUsersByName(@PathVariable String name) {
+        List<User> users = userService.findAllByName(name)
+        return users
+    }
 
 
 }
