4729083d7bbe7c75c72b4996665eb3ff1cc9d8a2
maxikyo <holovanovmax@gmail.com>
2023-08-26 18:50:38 +0200

    update1

--- src/main/groovy/com/holovanovmax/springboot_security_api/rest_api/model/UserDto.groovy
+++ src/main/groovy/com/holovanovmax/springboot_security_api/rest_api/model/UserDto.groovy
@@ -1,11 +1,20 @@
 package com.holovanovmax.springboot_security_api.rest_api.model
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+
 class UserDto {
 
+    @NotNull
+    @NotEmpty(message = "name can not be empty")
     String name
 
+    @NotNull
+    @NotEmpty(message = "password can not be empty")
     String password
 
+    @NotNull
+    @NotEmpty(message = "name can not be empty")
     String role
 
 }
