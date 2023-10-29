c69241ecebe56e4234c57a00d9ee88c021632103
maxikyo <holovanovmax@gmail.com>
2023-10-29 19:46:19 +0100

    update for test

--- src/test/groovy/com/holovanovmax/springboot_security_api/rest_api/service/UserServiceTest.groovy
+++ src/test/groovy/com/holovanovmax/springboot_security_api/rest_api/service/UserServiceTest.groovy
@@ -2,11 +2,9 @@ package com.holovanovmax.springboot_security_api.rest_api.service
 
 import com.holovanovmax.springboot_security_api.rest_api.model.BalanceOperation
 import com.holovanovmax.springboot_security_api.rest_api.model.User
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
-
 
 @SpringBootTest
 class UserServiceTest extends Specification {
@@ -14,8 +12,8 @@ class UserServiceTest extends Specification {
     @Autowired
     private UserServiceImpl userService
 
-    @Test
-    void "add money to userBalance"(){
+
+    def "add money to userBalance"() {
         given:
         User user = userService.registerNewUser(new User(
                 name: "name1",
