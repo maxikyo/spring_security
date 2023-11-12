03c5c94271488419d994a2b82ee1cbb179f81ea5
maxikyo <holovanovmax@gmail.com>
2023-11-13 00:22:26 +0100

    created userControllertest

--- src/test/groovy/com/holovanovmax/springboot_security_api/rest_api/controller/UserControllerTest.groovy
+++ src/test/groovy/com/holovanovmax/springboot_security_api/rest_api/controller/UserControllerTest.groovy
@@ -1,7 +1,16 @@
 package com.holovanovmax.springboot_security_api.rest_api.controller
 
 import com.holovanovmax.springboot_security_api.rest_api.contextLoader.ContextLoader
+import org.springframework.beans.factory.annotation.Autowired
 
 class UserControllerTest extends ContextLoader{
 
-}
+        @Autowired
+        UserController userController
+
+        def "when context is loaded then all expected beans are created"() {
+            expect: "the userController is created"
+            userController
+        }
+    }
+
